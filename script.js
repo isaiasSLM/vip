4c12a23c2ff745a26693848fd85a492505c6604a
Isaias silva <Isaiasprog@outlook.com>
2024-12-16 17:25:09 -0300

    atualizações

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function toggleMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('active');
 }
+
+// Função para fechar o menu
+function closeMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.remove('active');
+}
